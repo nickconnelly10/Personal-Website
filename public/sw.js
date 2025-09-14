456ebe04c9213b7b73c05eb3e3d36391a5120011
nickconnelly10 <nickconnelly10@gmail.com>
2025-09-14 15:07:39 -0400

    Fix empty page issue by updating service worker
    - Update service worker cache version to v2
    - Add cache clearing mechanism on activation
    - Add skipWaiting and clients.claim for immediate updates
    - This should resolve the empty page issue caused by cached content

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nicholasconnelly-box-v1'
+const CACHE_NAME = 'nicholasconnelly-box-v2'
 const urlsToCache = [
   '/',
   '/index.html',
@@ -16,6 +16,21 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    }).then(() => self.clients.claim())
   )
 })
 
